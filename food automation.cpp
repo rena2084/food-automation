7fe0bdbc3b50614f43eaedd5455a088f1103b642
Reza <rna13845492@gmail.com>
2025-04-17 17:01:10 +0330

    food automation v-0.0.1 four class were created

---
+++ food automation.cpp
@@ -0,0 +1,27 @@
+#include <iosteam>
+
+using namespace sdt;
+
+
+class Student(){
+
+};
+
+class Reservation(){
+
+};
+
+class Meal(){
+
+};
+
+class DiningHall(){
+
+};
+
+int main()
+{
+
+    return 0;
+
+}
