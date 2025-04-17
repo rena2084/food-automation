484a6fd1ac55b84011acb4a1e6f142511b1bcb64
Reza <rna13845492@gmail.com>
2025-04-17 17:25:33 +0330

    food automation v-0.0.3 The student class was defined

--- food automation.cpp
+++ food automation.cpp
@@ -6,19 +6,46 @@
 using namespace sdt;
 
 
-class Student(){
+class Student{
+private:
+    int _user_id;
+    string _student_id;
+    string _name;
+    string _email;
+    float _balance;
+    bool _is_active;
+public:
+    Student();
+    void print()const;
+    void reserve_meal(Meal);
+    bool cancel_reservation(Reservation);
+
+    //----- setters -----
+    void set_user_id();
+    void set_student_id();
+    void set_name();
+    void set_email();
+    void set_balance();
+    void set_is_active();
+
+    //----- getters -----
+    int get_user_id();
+    string get_student_id();
+    string get_name();
+    string get_email();
+    float get_balance();
+    bool get_is_active();
+};
+
+class Reservation{
 
 };
 
-class Reservation(){
+class Meal{
 
 };
 
-class Meal(){
-
-};
-
-class DiningHall(){
+class DiningHall{
 
 };
 
@@ -38,9 +65,9 @@ void gotoxy(int x, int y) {
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
-//------------------------ TextColor -------------------------
+//------------------------ textColor -------------------------
 
-void textColor(int x){
+void text_color(int x){
     HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(h, x);
 }
